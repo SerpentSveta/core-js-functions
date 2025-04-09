4fce86de1d60d16860082c7976eb6ee73ed50867
SerpentSveta <sveta.aleksandrova@gmail.com>
2025-04-09 14:12:35 +0300

    feat: add first part of functions

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    ignores: ["node_modules/"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+    },
+    linterOptions: {
+      noInlineConfig: true,
+    },
+    plugins: {
+      prettier: require("eslint-plugin-prettier"),
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
